994c21146a1b8ba4a894f869471d9714b39359ea
BrandonKakimoto <bmkakimoto@cpp.edu>
2025-08-26 15:15:26 -0700

    Create Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,15 @@
+/*
+ECE 2310 Fall 2025 - CPP
+Lab #1
+Brandon Kkaimoto 018075408
+8/26/2025
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
