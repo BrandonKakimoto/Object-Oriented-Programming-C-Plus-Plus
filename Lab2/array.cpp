762feb42709bf76bbe8c6ec6eefe46de40ec03e9
BrandonKakimoto <bmkakimoto@cpp.edu>
2025-10-22 22:24:10 -0700

    Completed Lab 2

--- Lab2/array.cpp
+++ Lab2/array.cpp
@@ -11,7 +11,7 @@ using namespace std;
  * @param size - number of elements in the array
  */
 
- void printMemArr(const int arr[],int size) { // void printMem(const int * arr, int size)
+ void printMemArr(const int arr[],int size) { // void printMem(const int *arr, int size)
     printf("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
     for(int i=0; i < size; i++) {
         printf("Value: %i at Memory Location %p\n", arr[i], arr + i);
